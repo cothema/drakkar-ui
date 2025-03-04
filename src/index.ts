56cd0e79ada0f31e4028e894e0b97f63ebb92b86
Milos Havlicek <miloshavlicek@gmail.com>
2025-03-04 17:26:01 +0100

    fix: Export WhatsApp Contact Chip Button to be able to use them

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ export {ChipButton} from "./stories/atoms/ChipButton/ChipButton";
 // Export Molecules
 export {ContactMailChipButton} from "./stories/molecules/ChipButton/ContactMailChipButton/ContactMailChipButton";
 export {ContactPhoneChipButton} from "./stories/molecules/ChipButton/ContactPhoneChipButton/ContactPhoneChipButton";
+export {ContactWhatsAppChipButton} from "./stories/molecules/ChipButton/ContactWhatsAppChipButton/ContactWhatsAppChipButton";
 
 // Export Tailwind CSS styles
 import "./styles/global.css";
