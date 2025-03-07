234a1b085cfecaace620aa32f94e48d879a6b31c
Milos Havlicek <miloshavlicek@gmail.com>
2025-03-07 13:51:39 +0100

    chore: index.ts component exports are now auto-generated using script

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-// Export Atoms
+// Auto-generated component exports
+
 export {ChipButton} from "./stories/atoms/ChipButton/ChipButton";
 export {YouTubeVideo} from "./stories/atoms/YouTubeVideo/YouTubeVideo";
-
-// Export Molecules
 export {ContactMailChipButton} from "./stories/molecules/ChipButton/ContactMailChipButton/ContactMailChipButton";
 export {ContactPhoneChipButton} from "./stories/molecules/ChipButton/ContactPhoneChipButton/ContactPhoneChipButton";
 export {
@@ -10,5 +9,5 @@ export {
 } from "./stories/molecules/ChipButton/ContactWhatsAppChipButton/ContactWhatsAppChipButton";
 export {YouTubeVideoSection} from "./stories/molecules/Sections/YouTubeVideoSection/YouTubeVideoSection";
 
-// Export Tailwind CSS styles
+// Export CSS styles
 import "./styles/global.css";
